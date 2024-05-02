444e11d3a765463564ca9596c4e1402cf81321bc
KL1cktm <urgzhenyok@mail.ru>
2024-05-02 22:31:47 +0300

    first changes

---
+++ lab1/logicBase/LogicBase.java
@@ -0,0 +1,68 @@
+package logicBase;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+
+public class LogicBase {
+    private String logicFunction;
+    private final List<Character> alphabet = List.of('0', '1', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+            'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+    private final List<String> operators = Arrays.asList("/\\", "\\/", "->", "!", "~", ")", "(");
+    private Stack<String> stack = new Stack<>();
+
+    public void checkImpracticabilityForm(String formula) { //проверка является ли формула невыполнимой
+        this.logicFunction = formula;
+        createReversPolishWrite();
+        System.out.println(stack);
+    }
+    private void createReversPolishWrite() { //создание таблицы истинности
+        Stack<String> operators = new Stack<>();
+        for (int i = 0; i < this.logicFunction.length(); i++) {   //перебор посимвольно логической функции
+            if (this.logicFunction.charAt(i) == ')') {  //проверка оператора закрытой скобки для записи ОПЗ
+                addOperatorsOnMainStack(operators);
+            } else {    //заполнение стека операндом
+                for (char token : this.alphabet) {
+                    if (token == this.logicFunction.charAt(i)) {
+                        this.stack.push(String.valueOf(token));
+                    }
+                }
+            }
+            if (this.logicFunction.charAt(i) == '(') { //проверка строки на символ (, для записи в сте операторов
+                operators.push("(");
+            } else {       //запись в стек операторов
+                for (String operator : this.operators) {
+                    if (operator.charAt(0) == this.logicFunction.charAt(i)) {
+                        if (operator.charAt(0) == '/') {     //проверка токена на конъюнкцию
+                            operators.push("/\\");
+                            i++;
+                            break;
+                        } else if (operator.charAt(0) == '\\') {  //проверка токена на дизъюнкцию
+                            operators.push("\\/");
+                            i++;
+                            break;
+                        } else if (operator.charAt(0) == '-') {  //проверка токена на импликацию
+                            operators.push(("->"));
+                            i++;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+    private void reverseElementsInStack() {
+
+    }
+    private void addOperatorsOnMainStack(Stack<String> operators) { //записываем в стек операндов операторы для создания ОПЗ
+        while (true) {
+            if (operators.peek().equals("(")) {
+                operators.pop();
+                return;
+            }
+            this.stack.push(operators.pop());
+        }
+    }
+}
--- lab1/logicBase/main.java
+++ lab1/logicBase/main.java
@@ -3,6 +3,9 @@ package logicBase;
 public class main {
     public static void main(String[] args)
     {
-
+        LogicBase logicBase = new LogicBase();
+//        logicBase.checkImpracticabilityForm("(A->(B/\\C))");
+        System.out.println("(((A->B)/\\(C\\/D))");
+        logicBase.checkImpracticabilityForm("(((A->B)/\\(C\\/D))");
     }
 }
